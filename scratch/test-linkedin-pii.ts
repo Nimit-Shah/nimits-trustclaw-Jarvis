bf995490a6e0adb08fd8e2735933239bacead2d2
Nimit Shah <nimitshah2503@gmail.com>
2026-07-17 03:05:15 +0530

    chore: remove LICENSE, scratch test files, CONTRIBUTING and CLAUDE docs

--- scratch/test-linkedin-pii.ts
+++
@@ -1,5 +0,0 @@
-import { scanForPII, extractStructuredPII } from "../src/server/api/routers/nimits-jarvis/agent/pii/pii-scanner";
-const text = "Check out my profile at https://www.linkedin.com/in/jackson-407499275 for more info.";
-const json = { vanityName: "jackson-407499275" };
-console.log("Text Scan:", scanForPII(text));
-console.log("JSON Extraction:", extractStructuredPII(json));
--- scratch/test-pii.ts
+++
@@ -1,3 +0,0 @@
-import { scanForPII } from "../src/server/api/routers/nimits-jarvis/agent/pii/pii-scanner";
-const text = "My linkedin urn is urn:li:person:123456789";
-console.log(scanForPII(text));
